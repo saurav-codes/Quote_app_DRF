d3ff5178fca388158a65f0deca2f865273c0cb53
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-09-03 08:29:28 -0700

    created class based API views

--- API/views.py
+++ API/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from API.serializer import QuoteSerializer
+from rest_framework import generics
+from API.models import Quote
 
 # Create your views here.
+
+class QuoteList(generics.ListCreateAPIView):
+    queryset= Quote.objects.all()
+    serializer_class= QuoteSerializer
+
+class Quote_ret_updt_dest(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Quote.objects.all()
+    serializer_class= QuoteSerializer
