e82fab21c855985c977ccc24af827a5b76c91ed5
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-09-03 09:38:06 -0700

    Fix: removed a bug in the urls raising error during PUT,PATCH,DELETE

--- API/urls.py
+++ API/urls.py
@@ -3,5 +3,5 @@ from API import views
 
 urlpatterns = [
     path('quotes',views.QuoteList.as_view()),
-    path('quote/<int:id>/',views.QuoteList.as_view()),
+    path('quote/<int:pk>',views.Quote_retrieve_update_destroy.as_view()),
 ]
