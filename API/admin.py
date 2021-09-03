03c1b0928aa1fbad8309da1a602f301bb31f4f08
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-09-03 07:56:33 -0700

    registered the model

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from API.models import Quote
 
 # Register your models here.
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display= ['id','quote_text','author']
