4a0384726dc579b6994c0121ccc5cb9ea4d4571c
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-09-04 19:59:41 -0700

    removed unused code:

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,21 +1,7 @@
-from django.http.response import HttpResponse 
-from scrapper.web_scrapper import scrap_quotes
-from API.serializer import QuoteSerializer
+from django.http.response import HttpResponse  
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('web scrapper')
+    return HttpResponse("Type ' < python manage.py category quantity > ' to scrap data")
 
-
-
-def quote_scraper(quantity=10, category='hello'):
-    # get the quotes
-    quotes_dict= scrap_quotes(category,quantity)
-    # saves to DB
-    if quotes_dict:
-        for key,value in quotes_dict.items():
-            serialized_data= QuoteSerializer(data={'quote_text':key,'author':value})
-            if serialized_data.is_valid():
-                serialized_data.save()
-                print('saved success')
