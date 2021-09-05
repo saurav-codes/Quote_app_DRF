eb55784358bcd20a42bb90d76df7f8f45b83ffb0
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-09-04 17:57:45 -0700

    added some temporary feature to run scrapper through url

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse 
+from scrapper.web_scrapper import scrap_quotes
+from API.serializer import QuoteSerializer
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('web scrapper')
+
+
+
+def quote_scraper(quantity=10, category='hello'):
+    # get the quotes
+    quotes_dict= scrap_quotes(category,quantity)
+    # saves to DB
+    if quotes_dict:
+        for key,value in quotes_dict.items():
+            serialized_data= QuoteSerializer(data={'quote_text':key,'author':value})
+            if serialized_data.is_valid():
+                serialized_data.save()
+                print('saved success')
